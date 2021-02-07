36f6d3741b8a96f57aa5d5109f0aa010e4861d62
Michel Pinho <jologpinho@gmail.com>
2021-02-07 00:26:04 -0300

    Update ex008.js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,4 +1,8 @@
-var vel = 70.8
-
-console.log(`A velocidade é de ${vel} Km/h`)
-
+var vel = 70
+if (vel > 60){
+    console.log(`A sua velocidade foi de ${vel} Km/h, acima do permitido. MULTADO`)  
+}
+else{
+    console.log(`A sua velocidade foi de ${vel} Km/h. Dentro do permitido, Parabéns.`)
+}
+console.log('Sempre dirija com educação e prudência')
