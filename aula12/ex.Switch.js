79db2365e0a11e8c767c13a9385ef04549741a6d
Michel Pinho <jologpinho@gmail.com>
2021-02-10 04:51:49 -0300

    Create ex.Switch.js

---
+++ aula12/ex.Switch.js
@@ -0,0 +1,27 @@
+var dia = new Date()
+dSemana = dia.getDay()
+
+
+switch(dSemana){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda-feira')
+        break
+    case 2:
+        console.log('terça-feira')
+        break
+    case 3:
+        console.log('Quarta-feira')
+        break
+    case 4:
+        console.log('Quinta-feira')
+        break
+    case 5:
+        console.log('Sexta-feira')
+        break
+    default:
+        console.log('Sábado')
+        break
+}
